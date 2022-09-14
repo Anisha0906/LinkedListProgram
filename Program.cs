4228dc07559d5286449dfa43b4047344efe8e38d
Anisha0906 <anishadas880@gmail.com>
2022-09-14 19:29:32 +0530

    Ability to create a simple Linked List

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+
+            foreach (int ele in list)
+            {
+             Console.WriteLine(ele);
+            }
+            Console.ReadLine();
+        }
+    }
+}
