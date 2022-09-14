3f7fa959814c534d0cbb47cb227e3b8639f7ed00
Anisha0906 <anishadas880@gmail.com>
2022-09-14 20:20:11 +0530

    Ability to create a new Linked List And appending 30 and 70 to 56

---
+++ CustomLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    internal class CustomLinkedList
+    {
+        Node head;
+
+        public void AddEnd(int newData)
+        {
+            Node newNode = new Node(newData);
+
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+        public void AddFirst(int newData)
+        {
+            Node newNode = new Node(newData);
+            newNode.next = head;
+            this.head = newNode;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,15 +10,32 @@ namespace LinkedListProgram
     {
         static void Main(string[] args)
         {
-            LinkedList<int> list = new LinkedList<int>();
-            list.AddLast(56);
-            list.AddLast(30);
-            list.AddLast(70);
+            //LinkedList<int> list = new LinkedList<int>();
+            //list.AddLast(56);
+            //list.AddLast(30);
+            //list.AddLast(70);
 
-            foreach (int ele in list)
-            {
-             Console.WriteLine(ele);
-            }
+            //foreach (int ele in list)
+            //{
+            //    Console.WriteLine(ele);
+            //}
+            //Console.ReadLine();
+
+           
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddFirst(70);
+            customLinkedList.AddFirst(30);
+            customLinkedList.AddFirst(56);
+            Console.WriteLine("Element added before head node 70");
+            customLinkedList.Display();
+
+            CustomLinkedList customLinkedList1 = new CustomLinkedList();
+            customLinkedList1.AddEnd(56);
+            customLinkedList1.AddEnd(30);
+            customLinkedList1.AddEnd(70);
+            Console.WriteLine();
+            Console.WriteLine("Element added after head node 56");
+            customLinkedList1.Display();
             Console.ReadLine();
         }
     }
